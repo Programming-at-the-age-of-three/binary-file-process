dd9bacbbf4029f3c05031fc9ad7eaa2e3f40e97d
xixi <xiiwuwu@163.com>
2019-05-19 17:15:09 +0800

    这就是作业，笔芯

--- Conan.py
+++ Conan.py
@@ -6,10 +6,14 @@ def tamper(student_id):
   lname=os.path.dirname(name)
   full_name=os.path.join(lname,"lenna.bmp")
   f=open("lenna.bmp", "r+b")
-  a=[2,0,1,8,1,1,1,1,3,0,2,7]
-  for l in a:
-      b=53
-      f.seek(b+l)
+  f.seek(54+2*3)
+  a=bytes([0,0,0])
+  f.write(a)
+  for l in range(1,12):
+      flag=int(student_id[l])
+      if flag==0:
+          flag=10
+      f.seek((flag-1)*3,1)
       a=bytes([0,0,0])
       f.write(a)
   f.close()
